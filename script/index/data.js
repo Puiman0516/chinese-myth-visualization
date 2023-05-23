da35b5c200f588c2502c935f34d3662c43a40300
Puiman0516 <lindachan0516@outlook.com>
2023-05-23 13:16:33 +0800

    Add data source

--- script/index/data.js
+++ script/index/data.js
@@ -1,11 +1,11 @@
 // svg元素
-const svg = d3.select('body').select('.timeLine').append('svg').attr('preserveAspectRatio', 'xMidYMid meet').attr('viewBox', '0 0 960 453')
+const svg = d3.select('body').select('.timeLine').append('svg').attr('preserveAspectRatio', 'xMidYMid meet').attr('viewBox', '0 0 960 428') // 960 453
 
 // 图表样式
 export const style = {
     // 神話人物标志
     myth: {
-        offset: [44, 370], // 位置(距svg边)
+        offset: [44, 360], // 位置(距svg边) 44, 370
         width: 25, // 寬
         height: 70, // 高
         angle: 12, // 令牌形狀角度
@@ -13,7 +13,7 @@ export const style = {
     },
     // 神话人物图片
     img: {
-        height: 300, // 高
+        height: 290, // 高 300
         width: 36, // 寬
         margin: 5, // 边框之間的間距
         padding: [20, -20], // 图与交互范圍的大小差
